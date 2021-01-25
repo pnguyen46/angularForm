9343472846f593b10686543617543fee710ef5f2
Megatron <squad@gmail.com>
2021-01-25 15:43:58 -0500

    create basic form

---
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css']
+})
+export class UserSettingsFormComponent implements OnInit {
+
+  pageTitle: string = 'Basic Angular Form';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
