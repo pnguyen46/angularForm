c1726c9c7fda50d1dc72683b112ecd3b44f447c1
Megatron <squad@gmail.com>
2021-01-26 14:55:13 -0500

    Data binding in Forms

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../data/user-setting';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -9,6 +10,16 @@ export class UserSettingsFormComponent implements OnInit {
 
   pageTitle: string = 'Basic Angular Form';
 
+  originalUserSettings: IUserSettings = {
+    name: "Tron",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "random texts for testing"
+  };
+
+  userSettings : IUserSettings = {...this.originalUserSettings};
+  
   constructor() { }
 
   ngOnInit() {
